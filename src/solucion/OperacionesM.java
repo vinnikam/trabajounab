116ee824b4a2436cdb14dca120bc9db4b398924c
vinnik4m <g.briceno@uniandes.edu.co>
2021-07-23 21:50:10 -0500

    Vinni- Desarrollo de operaciones matematicas version 2

--- src/solucion/OperacionesM.java
+++ src/solucion/OperacionesM.java
@@ -5,6 +5,9 @@
  */
 package solucion;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Vinni
@@ -13,6 +16,41 @@ public class OperacionesM {
     
     public int sumar(int num1, int num2){
         return num1+num2;
+    }
+    
+    public int leerNumeroSoloInt(String mensaje){
+        Scanner lector = new Scanner(System.in);
+        int num1;
+        do{
+            System.out.print(mensaje);
+            try {
+                num1 = lector.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("se devuelve un cero por error");
+                num1= 0;
+            }finally{
+                System.out.println("manejo error");
+            }
+            
+        }while(num1<0);
+        return num1;
+    }
+    public int leerNumero(String mensaje){
+        Scanner lector = new Scanner(System.in);
+        int num1;
+        String valorleido;
+        do{
+            System.out.print(mensaje);
+            try {
+                valorleido = lector.next();
+                num1 = Integer.parseInt(valorleido);
+                
+            }catch(NumberFormatException e){
+                num1= -1;
+            }
+            
+        }while(num1<0);
+        return num1;
     }
     
 }

fc22595fd335e9b74b775be4b1407f55274acc3a
vinnik4m <g.briceno@uniandes.edu.co>
2021-07-31 22:00:38 -0500

    Vinni- Encapsulamiento y mas de herencia

---
+++ src/Paqueteexterno/LlamandoClases.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paqueteexterno;
+
+import ejemploh.Usuario;
+
+/**
+ *
+ * @author Vinni
+ */
+public class LlamandoClases {
+    
+    public static void main(String[] args) {
+        Usuario u = new Usuario();
+        
+        u.asignoaNombre("Fernando");
+        
+        
+        
+        System.out.println(u.consultaNombre());
+    }
+    
+}

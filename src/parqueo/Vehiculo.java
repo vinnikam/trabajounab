41981cdba046d9ec149977f12cda25b230e8c7e0
Vinni <vinni9@gmail.com>
2021-08-05 21:58:03 -0500

    Vinni- Parqueo

--- src/parqueo/Vehiculo.java
+++ src/parqueo/Vehiculo.java
@@ -5,10 +5,55 @@
  */
 package parqueo;
 
+import java.util.Date;
+
 /**
  *
  * @author Vinni
  */
 public class Vehiculo {
+    private String placa;
+    private Date horaEntrada;
+    private Date horaSalida;
+    private long valorPagar;
+    private String tipo;
     
+    public Vehiculo(String placa, String tipo){
+        this.placa = placa;
+        this.tipo = tipo.trim().toUpperCase();
+        
+    }
+    //CONSULTAR PLACA
+    public String consultaPlaca(){
+        return this.placa;
+    }
+    //CONSULTAR TIPO
+    public String consultaTipo(){
+        return this.tipo;
+    }
+    
+    public Date consultaHoraEntrada(){
+        return this.horaEntrada;
+    }
+    
+    public void asignarHoraEntrada(Date horaEntrada){
+        this.horaEntrada = horaEntrada;
+    }
+    
+    public Date consultaHoraSalida(){
+        return this.horaSalida;
+    }
+    public void asignarHoraSalida(Date horaSalida){
+        this.horaSalida = horaSalida;
+    }
+    
+    public long consultaValorPagar(){
+        return this.valorPagar;
+    }
+    
+    public long calcularPago(int valorMinuto){
+        int minutos = 34;//difer hrafin-horafin; 
+        this.valorPagar = minutos*valorMinuto;
+        return this.valorPagar;
+    }
 }

387a36b0c1fa538856afc4cd254cab2e56f3809d
Vinni <vinni9@gmail.com>
2021-08-20 22:13:24 -0500

    Vinni- Proy Contactos BD

---
+++ src/bd/ManejadorConexion.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Vinni
+ */
+public class ManejadorConexion {
+    public Connection conectarse(){
+        String bd = "libreta";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/"+bd, "contacto", "clave");
+            return conexion;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ManejadorConexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ManejadorConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    } 
+    public Connection conectarseMDB(){
+        String bd = "libreta_bd";
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/"+bd, "contacto", "clave");
+            return conexion;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ManejadorConexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ManejadorConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+}
---
+++ src/bd/datosinicio.sql
@@ -0,0 +1,53 @@
+
+/**
+ * Author:  Vinni
+ * Created: 20/08/2021
+ */
+--Mysql 
+create database `libreta_bd`;
+CREATE USER 'contacto' IDENTIFIED BY 'clave';
+GRANT USAGE ON *.* TO 'contacto'@'%' IDENTIFIED BY 'clave';
+GRANT ALL privileges ON `libreta_bd`.* TO 'contacto'@'%';
+
+CREATE TABLE contactos
+(
+  identificador serial NOT NULL,
+  nombre text,
+  apellidos text,
+  telefono numeric(10),
+  correo text,
+  empresa text,
+  CONSTRAINT contactos_pkey PRIMARY KEY (identificador)
+)
+
+CREATE TABLE contactos (
+    identificador BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) ,
+    apellidos VARCHAR(100) ,
+    correo VARCHAR(50) ,
+    empresa VARCHAR(50) ,
+    telefono numeric(10,0),
+    favorito boolean);
+
+-- INSERCION
+insert into contactos (nombre,apellidos, correo, empresa, telefono, favorito) 
+values ('helios', 'fernandez', 'c@', 'rcn', 123, true);
+
+select * from contactos;
+
+select nombre, correo from contactos;
+
+update contactos set nombre = 'jorge';
+
+update contactos set nombre = 'jorge'
+where telefono = 123;
+
+update contactos set nombre = 'jorge'
+where telefono = 123;
+
+update contactos set nombre = 'PABLO'
+where identificador = 3;
+
+
+delete from contatos
+where identificador = 3;
